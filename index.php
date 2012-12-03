4cc79697c522378c2d8b101290563cd768f86199
mick <mick@mick-MT6833B.(none)>
2012-12-03 14:32:32 +0000

    update

--- index.php
+++ index.php
@@ -6,15 +6,16 @@ Country:Lithuania
 See:http://www.phpclasses.org/package/1314-PHP-Browse-a-given-directory.html
 */
 
-require_once 'explorer.class.php';
+require_once 'classes/autoload.php';
 $expl = new explorer;
-$expl->Set("root_dir","./");
-$expl->Set("icons_dir","icons/");
+$config = new config;
+$expl->Set("root_dir",$config->values->ROOT_DIR);
+$expl->Set("icons_dir",$config->values->ICON_DIR);
 $types['image.png'] = array ('jpg', 'gif');
 $types['png.png'] = array ('png');
 $types['php.png'] = array ('php');
 $expl->Set("types",$types);
-$expl->Set("un_icon","file.png");
-$expl->Set("dir_icon","directory.png");
+$expl->Set("un_icon",$config->values->UN_ICON);
+$expl->Set("dir_icon",$config->values->DIR_ICON);
 $expl->show_dirs();
 ?>
